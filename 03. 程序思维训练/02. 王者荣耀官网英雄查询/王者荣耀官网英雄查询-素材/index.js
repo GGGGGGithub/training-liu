1c8c335614e95dad44a1f81482f65047a5c6c4b4
liuchong <884368405@qq.com>
2022-04-29 19:06:14 +0800

    1

--- 03. 程序思维训练/02. 王者荣耀官网英雄查询/王者荣耀官网英雄查询-素材/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * 从网络获取当前的英雄数据
- * @returns Promise
- */
-async function getHeroes() {
-  return fetch('https://study.duyiedu.com/api/herolist')
-    .then((resp) => resp.json())
-    .then((resp) => resp.data.reverse());
-}
